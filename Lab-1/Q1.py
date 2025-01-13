6d71b1d0732da52d5837e8ddce09fd52e3742349
Anshika Srivastava <115107568+anshikasrivastava17@users.noreply.github.com>
2025-01-13 11:44:50 +0530

    basic arithmetic operations

---
+++ Lab-1/Q1.py
@@ -0,0 +1,13 @@
+''' Write a Python program to perform basic arithmetic operations 
+(addition, subtraction, multiplication, division, and modulus) on two numbers.
+Prompt the user to enter two numbers.
+Perform the arithmetic operations and print the results. 
+(Use appropriate operators and print formatting for clear output.) '''
+
+num1 = float(input("Enter the first number "))
+num2 = float(input ("Enter the second number "))
+print("Addition of two numbers",num1 + num2)
+print("Subtraction of two numbers",num1 - num2)
+print("Multiplication of two numbers",num1 * num2)
+print("Division of two numbers",num1 / num2)
+print("Modulus of two numbers",num1 % num2)
