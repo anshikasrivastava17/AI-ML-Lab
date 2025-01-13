6833a046386a16b660348c97efd288b21e39b6dd
Anshika Srivastava <115107568+anshikasrivastava17@users.noreply.github.com>
2025-01-13 21:32:41 +0530

    for , while loop

---
+++ Lab-1/Q5.py
@@ -0,0 +1,13 @@
+''' Write a program to print the numbers from 1 to 10 using both for and while loops.
+Use a for loop to iterate through a range of numbers.
+Use a while loop with a counter variable.'''
+
+print("Using for loop")
+for i in range(1,11):
+    print(i)
+    
+print("Using while loop")
+counter=0
+while(counter<=10):
+    print(counter)
+    counter+=1
