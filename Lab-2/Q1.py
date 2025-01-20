43c64f9ec8bb5b48a792287432ff359de9d937d1
Anshika Srivastava <115107568+anshikasrivastava17@users.noreply.github.com>
2025-01-20 10:44:50 +0530

    tuples

---
+++ Lab-2/Q1.py
@@ -0,0 +1,13 @@
+'''Q1.  Create and access tuples.
+Create a tuple of colors.
+Access elements using indexing.
+Try to modify an element in the tuple (to demonstrate immutability).
+Find the number of occurrences of a specific element in the tuple. '''
+
+colors = ("Red", "Violet", "Magenta", "Yellow", "Green", "Red","Black")
+print("First color:", colors[0])  #indexing  
+print("Last color:", colors[-1])
+print("Showing that tuple is immutable")
+#colors[3]="white"
+red_count = colors.count("Red")
+print("Number of times 'red' appears:", red_count)
